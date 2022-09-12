f91ef2aff5ba5159477b7ec2d4204a702e2c0a6c
Cesar Zabala <cesar@sunriseintegration.com>
2022-09-12 10:03:47 -0500

    Initial commit

---
+++ test/cdk-workshop.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkWorkshop = require('../lib/cdk-workshop-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-workshop-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
